c58f1df9ecddba0d8d835f799ba765ed886a2a89
Vinh Le <=>
2019-03-23 08:20:11 +0200

    Add api request to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Fib</h1>
-      </div>
-    );
-  }
+function App() {
+	const [seenIndexes, setSeenIndexes] = useState([])
+	const [values, setValues] = useState({})
+	const [index, setIndex] = useState('')
+
+	useEffect(() => {
+		const fetchSeenIndexes = async () => {
+			const seenIndexes = await axios.get('/api/values')
+			setSeenIndexes(seenIndexes.data)
+		}
+
+		fetchSeenIndexes()
+	}, [])
+
+	useEffect(() => {
+		const fetchValues = async () => {
+			const values = await axios.get('/api/values/current')
+			setValues(values.data)
+		}
+
+		fetchValues();
+	}, [])
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await axios.post('/api/values', {index})
+		setIndex('')
+	}
+
+	return(
+		<form onSubmit={handleSubmit}>
+			<h1>Fib calculator</h1>
+			<input
+				value={index}
+				onChange={(e) => setIndex(e.target.value)}
+			/>
+			<button type='submit' onClick={handleSubmit}>
+				Submit
+			</button>
+		</form>
+	)
 }
 
 export default App;
